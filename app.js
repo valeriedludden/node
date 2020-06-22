8575741632b72c3eab58b3f41bedf570a3d517eb
Valerie Ludden <valudden@gmail.com>
2020-06-22 08:55:07 -0600

    rate page

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("views", "views");
+app.set("view engine", "ejs");
 
 
 app.get("/", function(req, res) {
   console.log("Received a request for /");
+
 
 });
 
