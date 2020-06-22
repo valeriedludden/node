24e4d8d46933afcca84a82556be78eef4051e1b2
Valerie Ludden <valudden@gmail.com>
2020-06-21 22:36:12 -0600

    rate page

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-let rate = 0;
 
 app.get("/", function(req, res) {
   console.log("Received a request for /");
-  // res.render("home");
-  // res.end();
+  res.render("home");
+  res.end();
 });
 
 app.get("/home", function(req, res) {
@@ -25,12 +24,12 @@ app.get("/rate", function(req, res){
   res.render("rate", {rate: rate})
 });
 
-app.post("/rate", function(req, res){
-  let weight = req.body.weight;
-  let mail = req.body.mail;
-  // let newRate = {name: name, image: image};
-  res.redirect("/rate");
-});
+// app.post("/rate", function(req, res){
+//   let weight = req.body.weight;
+//   let mail = req.body.mail;
+//   // let newRate = {name: name, image: image};
+//   res.redirect("/rate");
+// });
 
 app.listen(PORT, function() {
   console.log("The server is up and listening on port 5000");
